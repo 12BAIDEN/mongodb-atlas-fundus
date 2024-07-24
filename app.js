88cb8322f45f450774586bc38f115c8270e41468
12BAIDEN <107093502+12BAIDEN@users.noreply.github.com>
2024-07-24 19:24:31 +0000

    commit

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
+// Middleware
 app.use(cors());
-
+app.use(express.json()); 
 
 
 const { MongoClient } = require('mongodb');
